10c377e1fb002a1c86675f3e6d5c31368690ae36
Denis Bubalo <bubalo2000@mail.ru>
2023-09-11 15:10:49 +0300

    Добавьте файлы проекта.

---
+++ SimpleTalk_GitHubOAuth22/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleTalk_GitHubOAuth2.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+    }
+}
