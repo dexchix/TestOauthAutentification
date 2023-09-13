a82dd19bbb9512465b75ce5c267fc9ce773259ac
Jabich <55812098+BubaloD@users.noreply.github.com>
2023-09-13 13:27:52 +0300

    7 commit

---
+++ CommitsManager/Controllers/ReposController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommitsManager.Controllers
+{
+    public class ReposController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public void GetCommits()
+        {
+
+        }
+    }
+}
